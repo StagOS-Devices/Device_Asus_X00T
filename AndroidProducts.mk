0c38ba65f38ffa2cbb3f4e15e9a6c1b3f27064f8
rafiqkn <sonu.rafiq786@gmail.com>
2020-07-13 18:01:59 +0530

    stagify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00T.mk
+    $(LOCAL_DIR)/stag_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-      lineage_X00T-userdebug
+      stag_X00T-userdebug
--- stag_X00T.mk
+++ stag_X00T.mk
@@ -20,7 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/stag/main.mk)
+
+# Bootanimation Res
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Gapps
+#TARGET_GAPPS_ARCH := arm64
+
+# Stag-Official 
+export BUILD_TYPE=OFFICIAL
 
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +37,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00T
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := lineage_X00T
+PRODUCT_NAME := stag_X00T
 PRODUCT_MODEL := Zenfone Max Pro M1
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
